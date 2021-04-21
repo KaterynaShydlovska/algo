45a375f217b5c2c7def152d25d24063173e0136a
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-04-20 21:10:06 -0500

    lopps

--- w1-d5/index.js
+++ w1-d5/index.js
@@ -17,3 +17,41 @@ function FizzBuzz(num) {
     }
 
 }
+
+// Print odds 1-20 - Using a loop write code that will console.log all of the odd values from 1 up to 20.
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+}
+// Print the sequence - Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+function sequence(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+sequence([4, 2.5, 1, -0.5, -2, -3.5])
+
+// Sigma - Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050in the end.
+
+function Sigma(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    console.log(sum)
+}
+// Factorial - Write code that will multiply values from 1 to some value n until the variable product is larger than 1 billion (1e8 for short). At the end console.log that value of n.
+
+function Factorial() {
+    let total = 1;
+    for (let n = 1; n < Number.MAX_VALUE; n++) {
+        total = total * n;
+        if (total > 1e8) {
+            console.log(n)
+        }
+    }
+}
+Factorial()
